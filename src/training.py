ad2585d6aefba718ecfbcf3a11be6c6d3b7d67b1
Datadriven1 <bhupendra.patel147@gmail.com>
2021-10-25 22:46:17 +0530

    updated

--- src/training.py
+++ src/training.py
@@ -1,6 +1,6 @@
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
-from src.utils.model import create_model, save_model, save_plot, CALLBACKS
+from src.utils.model import create_model, save_model, save_plot
 from src.utils.callbacks import get_callbacks
 import argparse
 import os
