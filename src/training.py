9ab4e3c58ed64eafc6432af784c8fa9701f19090
Datadriven1 <bhupendra.patel147@gmail.com>
2021-10-25 22:11:05 +0530

    updated

--- src/training.py
+++ src/training.py
@@ -1,6 +1,7 @@
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
 from src.utils.model import create_model, save_model, save_plot, CALLBACKS
+from src.utils.callbacks import get_callbacks
 import argparse
 import os
 import pandas as pd
@@ -23,7 +24,10 @@ def training(config_path):
     EPOCHS = config["params"]["epochs"]
     VALIDATION = (X_valid, y_valid)
 
-    history = model.fit(X_train, y_train, epochs = EPOCHS, validation_data= VALIDATION, callbacks=[CALLBACKS])
+    CALLBACK_LIST = get_callbacks(config, X_train)
+
+    history = model.fit(X_train, y_train, epochs = EPOCHS,
+     validation_data= VALIDATION, callbacks=[CALLBACK_LIST])
 
     artifacts_dir = config["artifacts"]["artifacts_dir"]
     model_dir = config["artifacts"]["model_dir"]
